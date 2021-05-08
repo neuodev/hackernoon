81c78218fc20421b00f491e884841d43e9132b7e
Ahmed Ibrahim <ib153507@gmail.com>
2021-05-08 18:20:42 +0200

    Add cookies when user login

--- src/modules/user/Login.ts
+++ src/modules/user/Login.ts
@@ -1,23 +1,31 @@
-import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
 import bcrypt from 'bcryptjs';
 import { User } from '../../entity/User';
 import LoginInput from './login/LoginInput';
+import { MyContext } from 'src/types/MyContext';
 @Resolver(User)
 class Login {
   @Mutation(() => User, {
     nullable: true,
   })
   async login(
-    @Arg('data') { password, email }: LoginInput
+    @Arg('data') { password, email }: LoginInput,
+    @Ctx() ctx: MyContext
   ): Promise<User | null> {
     const user = await User.findOne({
       where: {
         email,
       },
     });
+
+    // Validation
     if (!user) return null;
     const valid = await bcrypt.compare(password, user.password);
     if (!valid) return null;
+    //   send the sesstion
+    ctx.req.session!.userId! = user.id;
+   
+
     return user;
   }
 }
