c395113633e326c17c16017bb03945b3f2c6e648
Ahmed Ibrahim <ib153507@gmail.com>
2021-05-09 15:57:06 +0200

    test the confirm email resolver

--- src/modules/user/ConfirmEmailResolver.ts
+++ src/modules/user/ConfirmEmailResolver.ts
@@ -1,13 +1,27 @@
-import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
-import bcrypt from 'bcryptjs';
 import { User } from '../../entity/User';
-import { MyContext } from 'src/types/MyContext';
+import { Arg, Mutation } from 'type-graphql';
+import { redis } from '../../redis';
 
 class ConfirmEmailResover {
   @Mutation(() => Boolean, {
     nullable: false,
   })
   async confirmEamil(@Arg('token') token: string): Promise<boolean> {
+    const id = await redis.get(token);
+    if (!id) {
+      throw new Error('Token Expired');
+    }
+
+    const user = await User.findOne(id);
+
+    if (!user) throw new Error('User Not Found');
+
+    await redis.del(token);
+
+    user.confirmed = true;
+
+    user.save();
+
     return true;
   }
 }
